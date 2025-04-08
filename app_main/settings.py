f9bfc46d4e2cfd5d449d47bd51fcc6510ab4a838
Best-Doit <thor21evolution@gmail.com>
2025-04-08 19:02:09 -0400

    actualizacion de estaticos

--- app_main/settings.py
+++ app_main/settings.py
@@ -116,6 +116,8 @@ USE_TZ = True
 
 STATIC_URL = '/docs_pdf/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
