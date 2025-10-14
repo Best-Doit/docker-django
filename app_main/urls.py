9b04f6ae9b72b21a8fa81d1b5b7d0ff309b6d362
Best-Doit <thor21evolution@gmail.com>
2025-10-14 17:33:23 -0400

    Fix: Agregar URLs específicas para robots.txt y favicon.ico para eliminar 404s

--- app_main/urls.py
+++ app_main/urls.py
@@ -15,7 +15,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.http import HttpResponse
+import os
+
+def robots_txt(request):
+    """Servir robots.txt"""
+    robots_path = os.path.join(settings.STATIC_ROOT, 'robots.txt')
+    if os.path.exists(robots_path):
+        with open(robots_path, 'r') as f:
+            content = f.read()
+        return HttpResponse(content, content_type='text/plain')
+    return HttpResponse('User-agent: *\nDisallow: /', content_type='text/plain')
+
+def favicon_ico(request):
+    """Servir favicon.ico"""
+    favicon_path = os.path.join(settings.STATIC_ROOT, 'favicon.ico')
+    if os.path.exists(favicon_path):
+        with open(favicon_path, 'rb') as f:
+            content = f.read()
+        return HttpResponse(content, content_type='image/x-icon')
+    return HttpResponse(status=404)
 
 urlpatterns = [
     path('', include('docs_pdf.urls')),
+    path('robots.txt', robots_txt, name='robots_txt'),
+    path('favicon.ico', favicon_ico, name='favicon_ico'),
 ]
